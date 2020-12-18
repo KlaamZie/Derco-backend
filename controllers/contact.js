8a63bc5c0b354a674c8baf5b55e5691b835424ea
Enzo Viry <enzo.viry@gmail.com>
2020-12-18 16:47:56 +0100

    fix env

--- controllers/contact.js
+++ controllers/contact.js
@@ -6,7 +6,7 @@ async function contactMail(name, mail, phone, type, message) {
     const transporter = nodemailer.createTransport({
         host: process.env.MAIL_HOST,
         port: process.env.MAIL_PORT,
-        secure: process.env.NODE_ENV !== "production" ? false : true,
+        secure: false,
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASS
